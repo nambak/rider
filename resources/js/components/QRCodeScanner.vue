379b25a24e0e1efca4a90c5ac2a96558dd97f1ba
nambak80 <alex@10minutesquad.com>
2021-09-23 17:27:15 +0900

    add display errror message

--- resources/js/components/QRCodeScanner.vue
+++ resources/js/components/QRCodeScanner.vue
@@ -2,6 +2,7 @@
     <div>
         <h1 class="text-center">주문서 QR코드 스캔</h1>
         <qrcode-stream @decode="onDecode" :track="paintOutline" @init="onInit"></qrcode-stream>
+        <p class="error">{{ error }}</p>
         <b-card title="주문내역" v-if="data" class="mt-3">
             <b-card-text>
                 <p>주문번호: {{ data.order_id }}</p>
@@ -27,6 +28,7 @@ export default {
     data() {
         return {
             data: null,
+            error: '',
         }
     },
 
@@ -35,11 +37,28 @@ export default {
             this.getOrderDetails(decodedString);
         },
 
-        onInit(promise) {
-            console.log('camera init');
-            promise.catch(error => {
-                console.error(error.message);
-            });
+        async onInit (promise) {
+            try {
+                await promise
+            } catch (error) {
+                if (error.name === 'NotAllowedError') {
+                    this.error = "ERROR: you need to grant camera access permission"
+                } else if (error.name === 'NotFoundError') {
+                    this.error = "ERROR: no camera on this device"
+                } else if (error.name === 'NotSupportedError') {
+                    this.error = "ERROR: secure context required (HTTPS, localhost)"
+                } else if (error.name === 'NotReadableError') {
+                    this.error = "ERROR: is the camera already in use?"
+                } else if (error.name === 'OverconstrainedError') {
+                    this.error = "ERROR: installed cameras are not suitable"
+                } else if (error.name === 'StreamApiNotSupportedError') {
+                    this.error = "ERROR: Stream API is not supported in this browser"
+                } else if (error.name === 'InsecureContextError') {
+                    this.error = 'ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.';
+                } else {
+                    this.error = `ERROR: Camera error (${error.name})`;
+                }
+            }
         },
 
         paintOutline (detectedCodes, ctx) {
